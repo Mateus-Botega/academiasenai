ff045ebc78736c8b2cd225fc0d9a95bc15a2051a
unknown <matibotega@utlook.com>
2022-03-17 20:07:27 -0300

    Inicio Do Desenvolvimento Dos Recursos de Gestão de Instrutores e Equipamentos

---
+++ src/br/com/senai/academiasenai/entity/Instrutor.java
@@ -0,0 +1,23 @@
+package br.com.senai.academiasenai.entity;
+
+public class Instrutor {
+
+	private Integer ID;
+	private String nomeCompleto;
+	
+	public Integer getID() {
+		return ID;
+	}
+	public void setID(Integer iD) {
+		ID = iD;
+	}
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	
+	
+	
+}
